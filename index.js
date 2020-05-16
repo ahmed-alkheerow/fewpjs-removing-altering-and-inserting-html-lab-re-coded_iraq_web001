8c9fb9da37d62653550e543d3590e5d3244ac625
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-16 03:32:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Write your code here!
 let element = document.createElement('div');
 document.body.appendChild(element);
+
+
 let ul = document.createElement('ul')
 
 for (let i = 0; i < 3; i++) {
