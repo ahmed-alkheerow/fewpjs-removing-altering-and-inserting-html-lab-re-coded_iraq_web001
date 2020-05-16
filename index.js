f6ce190c8f34e5b8b9dccccbd2584683d67f5fb0
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-16 03:42:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 // Write your code here!
-let element = document.createElement('div');
-document.body.appendChild(element);
-
-
-let ul = document.createElement('ul')
-
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
-
-element.appendChild(ul)
+// let mainId = document.querySelector(main#main);
+// main.remove();
+main#main.remove()
